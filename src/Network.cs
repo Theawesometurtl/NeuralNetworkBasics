bfa3bfeb6abbaaa81e0bde4e9fb137c35eb00d6f
Theawesometurtl <shablammmo@gmail.com>
2022-11-05 14:07:59 -0700

    yess it works, now I just need to do smt with it

--- src/Network.cs
+++ src/Network.cs
@@ -7,46 +7,58 @@ public String net = "1234567891011121314151617181920212223242526272829";
 
 //I'm using charInString as a shared counter when running the network. It tells the network which character in the string it's on
 private int charInString = 0;
+//same with column
+private int column = 0;
 
-public int[] Run(int[] inputs, int column) {
+public int[] Run(int[] inputs) {
+    inputs = AddWeights(inputs, 0);
     
-    //doesn't quite work yet
-    inputs = AddWeights(inputs, column, 0);
-
     if (netLayout.Length != column+1)
     {
         int[] newInputs = new int[netLayout[column+1]];
-        for (int row = 0; row < netLayout[column]; row++)
-        {
-            //I know this nested loop layout isn't at all readable, but I'm lazy and don't want to split it into seperate functions
-            for (int bias = 0; bias < netLayout[column+1]; bias++)
-            {
-                newInputs[bias] = 0;
-                char c = net[charInString];
-                charInString++;
-                newInputs[bias] += (int) c; 
-                Console.WriteLine(c);
-            }
-        }
-        inputs = newInputs;
+        inputs = AddBiases(inputs, newInputs, 0, 0);
     }
     //my first ever recursive program! (I had 4 nested for loops)
     column++;
-    if (column !< netLayout.Length) {
+    if (column >= netLayout.Length) {
+        charInString = 0;
+        column = 0;
         return inputs;
     }
-    return Run(inputs, column);
+    return Run(inputs);
 }
 
-private int[] AddWeights(int[] inputs, int column, int weightNum) {
+private int[] AddWeights(int[] inputs, int weightNum) {
     char c = net[charInString];
     charInString++;
     inputs[weightNum] *= (int) c;
     Console.WriteLine(c);
-    if (weightNum !< netLayout[column]) {
+
+    weightNum++;
+    if (weightNum >= netLayout[column]) {
         return inputs;
     }
-    return AddWeights(inputs, column, weightNum++);
+    return AddWeights(inputs, weightNum);
+}
+
+
+private int[] AddBiases(int[] inputs, int[] newInputs, int weightNum, int biasNum) {
+    newInputs[biasNum] = 0;
+    char c = net[charInString];
+    charInString++;
+    newInputs[biasNum] += (int) c + inputs[weightNum]; 
+    Console.WriteLine(c);
+
+    biasNum++;
+    if (biasNum >= netLayout[column+1]) {
+        weightNum++;
+        biasNum = 0;
+        if (weightNum >= netLayout[column]) {
+            return newInputs;
+        }
+    }
+    return AddBiases(inputs, newInputs, weightNum, biasNum);
+
 }
 
 //mutates the network string
